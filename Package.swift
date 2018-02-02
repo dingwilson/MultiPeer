2fede7736419b48cd3842b47ac3665ff11cacf38
Wilson Ding <hello@wilsonding.com>
2018-02-02 10:29:40 -0600

    Added individual schemes for MacOS and iOS

--- Package.swift
+++ Package.swift
@@ -3,20 +3,4 @@
 import PackageDescription
 
 let package = Package(
-    name: "MultiPeer",
-    products: [
-        .library(
-            name: "MultiPeer",
-            targets: ["MultiPeer"])
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "MultiPeer",
-            dependencies: []),
-        .testTarget(
-            name: "MultiPeerTests",
-            dependencies: ["MultiPeer"])
-    ]
-)
+    name: "MultiPeer")
