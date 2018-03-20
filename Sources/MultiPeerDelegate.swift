47f4b76ed796921907c6cfd008fa2d08f1f08e75
Wilson Ding <hello@wilsonding.com>
2018-03-20 11:49:07 -0500

    Modified MultiPeerDelegate protocol to be public (#6)

--- Sources/MultiPeerDelegate.swift
+++ Sources/MultiPeerDelegate.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
-protocol MultiPeerDelegate: class {
+/// MultiPeerDelegate
+///
+/// - Delegate for MultiPeer. Conform to this interface to recieve data and be notified when connection/disconnection events occur
+///
+/// multiPeer(didRecieveData: Data, ofType: UInt32)
+/// multiPeer(connectedDevicesChanged: [String])
+
+public protocol MultiPeerDelegate: class {
 
     /// didReceiveData: delegate runs on receiving data from another peer
     func multiPeer(didReceiveData data: Data, ofType type: UInt32)
