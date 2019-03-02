2fd2104b6a3a5780cd76171f1c60bc32350b14f4
ioviov <einesaps@gmail.com>
2019-03-02 18:22:28 +0200

    Added support for tvOS (#20)
    * Added support for tvOS
    
    * Added target tvOS and minimum tvOS version

---
+++ MultiPeer tvOS/MultiPeer_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  MultiPeer_tvOS.h
+//  MultiPeer tvOS
+//
+//  Created by Martynets Ruslan on 3/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiPeer_tvOS.
+FOUNDATION_EXPORT double MultiPeer_tvOSVersionNumber;
+
+//! Project version string for MultiPeer_tvOS.
+FOUNDATION_EXPORT const unsigned char MultiPeer_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiPeer_tvOS/PublicHeader.h>
+
+
